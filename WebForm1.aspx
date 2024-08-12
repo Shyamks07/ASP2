4c8eb3fe4b61c92b9167cdbbf146772ed895fe69
Shyamks07 <97223205+Shyamks07@users.noreply.github.com>
2024-08-12 09:36:45 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Save" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="update" />
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+using System.Configuration;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack!=true)
+            {
+                show();
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int eid = Convert.ToInt16(TextBox1.Text);
+            string n = TextBox2.Text;
+            int sal = Convert.ToInt16(TextBox3.Text);
+          string Constring =ConfigurationManager.ConnectionStrings["employee1"].ConnectionString;
+            SqlConnection con = new SqlConnection(Constring);
+            con.Open();
+            string sqlstmt = "insert into employee1(empid,firstname,salary)values(@empid,@firstname,@salary)";
+            SqlCommand cmd = new SqlCommand(sqlstmt, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Parameters.AddWithValue("@empid", eid);
+            cmd.Parameters.AddWithValue("@firstname", n);
+            cmd.Parameters.AddWithValue("@salary", sal);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            show();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int a= Convert.ToInt16(TextBox4.Text);
+            string Constring = ConfigurationManager.ConnectionStrings["employee1"].ConnectionString;
+            SqlConnection con = new SqlConnection(Constring);
+            con.Open();
+            string sqlstm = "delete from  employee1 where empid=@empid";
+            SqlCommand cmd = new SqlCommand(sqlstm, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Parameters.AddWithValue("@empid", a);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            show();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int x= Convert.ToInt32(TextBox5.Text);
+            int d = Convert.ToInt16(TextBox6.Text);
+            string Constring = ConfigurationManager.ConnectionStrings["employee1"].ConnectionString;
+            SqlConnection con = new SqlConnection(Constring);
+            con.Open();
+            string sqlstm = "update employee1 set salary=@s where empid=@d";
+            SqlCommand cmd = new SqlCommand(sqlstm, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Parameters.AddWithValue("@s", x);
+            cmd.Parameters.AddWithValue("@d", d);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            show();
+        }
+
+
+        public void show()
+        {
+            string Constring = ConfigurationManager.ConnectionStrings["employee1"].ConnectionString;
+            SqlConnection con = new SqlConnection(Constring);
+            con.Open();
+            string sqlstm = "select * from employee1";
+
+            SqlDataAdapter adp = new SqlDataAdapter(sqlstm, con);
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            con.Close();
+        }
+    }
+}
